fda23dd859127c8de14819c8ff98773e6b225d06
David Granjon <dgranjon@ymail.com>
2020-02-29 02:35:53 +0100

    hack for modules

--- inst/glouton.js
+++ inst/glouton.js
@@ -1,18 +1,18 @@
 $(document).on('shiny:connected', function(event) {
-  function sendCookies(){
+  function sendCookies(id){
     var res = Cookies.get();
-    Shiny.setInputValue("gloutoncookies", res, {priority: "event"});
+    Shiny.setInputValue(id + "-gloutoncookies", res, {priority: "event"});
   }
 
   sendCookies();
 
-  Shiny.addCustomMessageHandler('fetchcookie', function(name) {
-    var res = Cookies.get(name);
-    Shiny.setInputValue("gloutoncookie", res, {priority: "event"});
+  Shiny.addCustomMessageHandler('fetchcookie', function(arg) {
+    var res = Cookies.get(arg.name);
+    Shiny.setInputValue(arg.ns + "-gloutoncookie", res, {priority: "event"});
   });
 
   Shiny.addCustomMessageHandler('fetchcookies', function(arg) {
-    sendCookies();
+    sendCookies(arg.ns);
   });
 
   Shiny.addCustomMessageHandler('addcookie', function(arg) {
